f6517d88c3df70fdd7c04973140dfe01079e6b11
Lewis Lebentz <LewisLebentz@users.noreply.github.com>
2019-07-03 14:14:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='truffleHogRegexes',
-    version='0.0.7',
+    version='0.0.8',
     description='These regexes power truffleHog.',
-    url='https://github.com/dxa4481/truffleHogRegexes',
+    url='https://github.com/LewisLebentz/truffleHogRegexes',
     author='Dylan Ayrey',
     author_email='dylanayrey@gmail.com',
     license='GNU',
